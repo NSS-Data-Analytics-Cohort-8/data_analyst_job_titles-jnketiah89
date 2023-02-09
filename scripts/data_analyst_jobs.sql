88140d8cfd8278e95cf92ca3d8beebbfc6094c76
Jasmine Nketiah <jasmine.nketiah@gmail.com>
2023-02-09 17:28:19 -0600

    working on Q11

--- scripts/data_analyst_jobs.sql
+++ scripts/data_analyst_jobs.sql
@@ -24,7 +24,7 @@ WHERE LOCATION = 'TN';
 --Answer: 21
 
 --3.How many are there in either Tennessee or Kentucky?
-SELECT COUNT 
+SELECT COUNT (*)
 FROM data_analyst_jobs
 WHERE LOCATION = 'TN' OR LOCATION ='KY';
 
@@ -47,8 +47,11 @@ WHERE review_count BETWEEN 500 AND 1000;
 
 
 -- 6.Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?
-SELECT AVG(star_rating) 
-FROM data_analyst_jobs;
+SELECT location, AVG(star_rating) 
+FROM data_analyst_jobs
+GROUP BY location;
+
+--Answer:Nevada
 
 -- 7.Select unique job titles from the data_analyst_jobs table. How many are there?
 SELECT DISTINCT tittle
@@ -59,16 +62,22 @@ FROM data_analyst_jobs;
 -- 8.How many unique job titles are there for California companies?
 SELECT DISTINCT COUNT(*) tittle
 FROM data_analyst_jobs
-WHERE location = 'California';
+WHERE location = 'CA';
+
+--Answer:376
 
 
--- Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
--- Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+-- 9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+SELECT AVG(star_rating) 
+FROM data_analyst_jobs.company
+HAVING review_count > 5000;
 
--- Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+-- 10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
--- How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+-- 11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+
+-- 12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
 -- BONUS: You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
 
